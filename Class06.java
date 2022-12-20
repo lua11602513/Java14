f5d8ea414e4093482e8323e7acfdd100e952f8b3
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-12-21 00:46:31 +0800

    Add files via upload

---
+++ Class06.java
@@ -0,0 +1,16 @@
+import java.io.*;
+	public class Class06 {
+		public static void main(String args[]){
+try{
+	char data[]=new char[128];
+	FileReader fr=new FileReader("c:\\java\\train.txt");
+	int num=fr.read(data);
+	String str=new String(data,0,num);
+	System.out.println("Characters read= "+num);
+	System.out.println(str);
+	fr.close();
+}
+	catch(IOException e){
+}
+ }
+ } 
