18e9e3449911a6eb29988f10277c7aecc317cbec
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-12-21 01:03:23 +0800

    Add files via upload

---
+++ Class11.java
@@ -0,0 +1,17 @@
+import java.io.*;
+	public class Class11{
+		public static void main(String args[]) throws IOException{
+			FileInputStream fi=new FileInputStream("c:\\java\\number.txt");
+			byte data[]=new byte[fi.available()];
+			int count=0;
+			fi.read(data);
+			String str=new String(data);
+			System.out.println(str);
+			char arr[]=str.toCharArray();
+	for(int i=0;i<arr.length;i++)
+		if(arr[i]=='\r')
+			count++;
+			System.out.println((count+1)+" lines read");
+			fi.close();
+}
+ } 	
